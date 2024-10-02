a4e4c5d6e54a13834351120e9123adaf5ee07a05
omjdreams <om.javheri@dreamsinternational.in>
2024-10-02 20:56:38 +0530

    upd

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: { outDir: '..' },
+  // needed for github pages just put the repo name here
+  base: 'messageFrontend', 
 })
